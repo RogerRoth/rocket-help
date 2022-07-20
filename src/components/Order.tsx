0bd7712a5632c68b0279b099d9fddfcae9a4d0d2
RogerRoth <rogr_roth@yahoo.com.br>
2022-07-20 15:43:48 -0300

    Dev - Integrated with backend and added the screens functionalities

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -31,7 +31,7 @@ export function Order({ data, ...rest }: Props) {
 
         <VStack flex={1} my={5} ml={5}>
           <Text color="white" fontSize="md">
-            Patrimôno {data.patrimony}
+            Patrimônio {data.patrimony}
           </Text>
 
           <HStack alignItems="center">
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 import { Heading, VStack, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -9,14 +10,39 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn(){
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
 
   const { colors } = useTheme();
 
   function handleSignIn(){
-    console.log(email)
-    console.log(password)
+    if(!email || !password){
+      return Alert.alert('Entrar', 'Informe E-mail e Senha')
+    }
+
+    setIsLoading(true);
+
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      setIsLoading(false);
+
+      if (error.code === 'auth/invalid-email'){
+        return Alert.alert('Entrar', 'E-mail inválido.')
+      }
+
+      if (error.code === 'auth/wrong-password'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválido.')
+      }
+
+      if (error.code === 'auth/user-not-found'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválido.')
+      }
+
+      return Alert.alert('Entrar', 'Não foi possivel entrar.')
+    })
   }
 
   return(
@@ -38,12 +64,14 @@ export function SignIn(){
         InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
         secureTextEntry
         onChangeText={setPassword}
+        onSubmitEditing={handleSignIn}
       />
 
       <Button 
         title='Entrar' 
         w="full"
         onPress={handleSignIn}
+        isLoading={isLoading}
       />
     </VStack>
   )
